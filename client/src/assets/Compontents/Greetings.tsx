855da44168a14c1aa3cd18dc06b8527648b90495
Kayhan Mamak <kayhanturk90@live.com>
2025-09-17 14:19:51 +1000

    text chat box, tailwind.css working

---
+++ client/src/assets/Compontents/Greetings.tsx
@@ -0,0 +1,12 @@
+export function Greeting() {
+
+    return(
+        <div className="flex items-center shadow-xl bg-white-500 text-black px-4 py-2 rounded 
+                   transform transition-transform duration-300 
+                   hover:-translate-y-1 drop-shadow-lg">
+    <h1>
+        Welcome User How can i help you today?
+    </h1>
+    </div>
+)
+}
