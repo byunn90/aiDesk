dcd9a46af10c2324f96c1fc14232766fa1c80ca8
Kayhan Mamak <kayhanturk90@live.com>
2025-09-22 20:09:20 +1000

    NavBar Structure done

--- client/src/assets/Compontents/Greetings.tsx
+++ client/src/assets/Compontents/Greetings.tsx
@@ -4,7 +4,7 @@ export function Greeting() {
       className="flex justify-center items-center max-w-xl mx-auto 
                  bg-white text-black px-6 py-4 rounded-2xl shadow-lg 
                  transform transition-all duration-300 
-                 hover:-translate-y-1 hover:shadow-2xl mt-15"
+                 hover:-translate-y-1 hover:shadow-2xl mt-10"
     >
       <h1 className="text-xl font-semibold text-center">
         Welcome User, how can I help you today?
