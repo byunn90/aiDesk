daac2767649368507cbd3d4c76cc5c033e1b5c9a
Kayhan Mamak <kayhanturk90@live.com>
2025-09-22 15:22:29 +1000

    Navbar today

--- client/src/assets/Compontents/Chat.tsx
+++ client/src/assets/Compontents/Chat.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react"
+import React, { useRef, useState } from "react"
 
 export function Chat() {
   const [input, setInput] = useState("")
   const [messages, setMessages] = useState<{id: string; role:"user"|"assistant"; content:string}[]>([])
-
+  const scrollRef = useRef<HTMLDivElement>(null)
   const onSubmit = (e: React.FormEvent) => {
   e.preventDefault()
   const text = input.trim()
@@ -17,22 +17,23 @@ export function Chat() {
 
   return(
     <>
-    <div className="m-20 whitespace-pre-wrap flex items-center w-full max-w-3xl mb-3 space-y-2 font-mono bg-gray-50">
-      {messages.map(m => {
-      const isUser = m.role === "user"
-      const rowClass = isUser ? "justify-end" : "justify-start"
-      const bubbleClass = isUser
-      ? "bg-gray-600 text-white"
-      : "bg-zinc-100 text-zinc-900"
-        return (<div key={m.id} className={`flex ${rowClass}`}>
-          <div className={`max-w-[75%] px-4 py-2 rounded-2xl shadow ${bubbleClass}`}>
-            {m.content}
-          </div>
+<div
+  ref={scrollRef}
+  className="h-150 overflow-y-auto whitespace-pre-wrap flex flex-col w-full max-w-3xl space-y-2 px-4"
+>
+  {messages.map(m => {
+    const isUser = m.role === "user"
+    const rowClass = isUser ? "justify-start" : "justify-end"
+    const bubbleClass = isUser ? "bg-gray-600 text-white" : "bg-zinc-100 text-zinc-900"
+    return (
+      <div key={m.id} className={`flex ${rowClass}`}>
+        <div className={`max-w-[75%] px-4 py-2 rounded-2xl shadow ${bubbleClass}`}>
+          {m.content}
         </div>
-        )
-      })}
-    </div>
-    
+      </div>
+    )
+  })}
+</div>
     <form onSubmit={onSubmit} className="flex items-center gap-2 w-full max-w-3xl p-3 border rounded-xl">
     <input
      className="flex-1 min-w-0 bg-transparent outline-none"
